763cd3ee6d6803ae75093c8bfabb9e5d2acb13af
fonsiher <ehernandezm@est.ups.edu.ec>
2021-06-25 12:02:31 -0500

    Formulario de Contacto

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const hbs = require('hbs');
+const nodemailer = require("nodemailer")
 require('./hbs/helpers');
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use(require(__dirname + '/views/parciales/send-email'));
+//app.use(require(__dirname + '/views/parciales/send-email'));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -29,6 +31,45 @@ app.get('/portfolio-details', function(req, res) {
 app.get('/success', function(req, res) {
     res.render('success', {});
 });
+
+
+app.post('/send-email', (req, res) => {
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'lillian75@ethereal.email',
+            pass: 'yT2EK6XGjRMA4ZNXDb'
+        }
+    });
+    var mailOptions = {
+        from: req.body.name,
+        to: req.body.email,
+        subject: req.body.subject,
+        text: req.body.message
+    }
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            res.render('index', {
+                alert: true,
+                alertTitle: 'Error al enviar',
+                alertMessage: error,
+                icon: 'error',
+                timer: 1500
+            })
+        } else {
+            res.render('index', {
+                alert: true,
+                alertTitle: 'Mensaje Enviado',
+                icon: 'success',
+                timer: 1500
+            })
+        }
+    })
+
+
+})
+
 
 app.listen(port, () => {
     console.log("Servidor Iniciado, escuchando en el puerto 3000");
